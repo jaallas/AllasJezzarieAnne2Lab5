1232cd82df9b5e7ab3b8e6ff066655ea9f509da3
jaallas <jezzarieanne.allas.iics@ust.edu.ph>
2018-10-14 00:18:28 +0800

    Initial Commit

--- app/src/main/java/com/allas/jezzarie/Main2Activity.java
+++ app/src/main/java/com/allas/jezzarie/Main2Activity.java
@@ -1,7 +1,11 @@
 package com.allas.jezzarie;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        Button act1 = (Button) findViewById(R.id.act1);
+        act1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent act1 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(act1);
+
+
+            }
+        });
+
+
+        Button map = (Button) findViewById(R.id.map2);
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:14.6096767,120.9874574");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(mapIntent);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/allas/jezzarie/MainActivity.java
+++ app/src/main/java/com/allas/jezzarie/MainActivity.java
@@ -1,6 +1,7 @@
 package com.allas.jezzarie;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(act1);
             }
         });
+
+        Button map = (Button) findViewById(R.id.map);
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:14.6096767,120.9874574");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(mapIntent);
+                }
+            }
+            });
 
     }
 }
